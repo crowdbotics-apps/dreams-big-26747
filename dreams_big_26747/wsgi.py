40a3f8447a986b0bf956c32562f8ee73700bfd41
Crowdbotics <team@crowdbotics.com>
2021-05-16 13:30:21 +0000

    Initial commit.

---
+++ dreams_big_26747/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for dreams_big_26747 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dreams_big_26747.settings')
+
+application = get_wsgi_application()
